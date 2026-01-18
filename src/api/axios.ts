ef71deb844a71477362742a855fc1dc590fb707d
envisiontechsol <jaisimha@envisiontechsol.in>
2026-01-18 23:47:01 +0530

    Fix login API path to /api/auth/login

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://quizapp-backend.onrender.com/api",
+   baseURL: "https://quizapp-backend-e6yh.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
